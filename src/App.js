37da8754099df049f5c486b8507c49c0b5eb76b8
David Boland <dboland77@gmail.com>
2020-08-28 23:25:32 +0100

    Added movie flexbox and hover overlays

--- src/App.js
+++ src/App.js
@@ -1,22 +1,55 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import BlackMirror from "./images/BlackMirror.jpg";
+import BreakingBad from "./images/BreakingBad.jpg";
+import DeathNote from "./images/DeathNote.jpg";
+import GOT from "./images/GOT.jpg";
+import TheWire from "./images/TheWire.jpg";
+import WalkingDead from "./images/WalkingDead.jpg";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Welcome to Kodflix</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='container'>
+        <div className='divItem'>
+          <img src={BlackMirror} alt="Black Mirror" />
+          <div className = 'overlay'>
+            <h1>Black Mirror</h1>
+          </div>
+        </div>
+        <div className='divItem'>
+          <img src={BreakingBad} alt="Breaking Bad" />
+          <div className = 'overlay'>
+            <h1>Breaking Bad</h1>
+         </div>
+        </div>
+        <div className='divItem'>
+          <img src={DeathNote} alt="Death Note" />
+          <div className = 'overlay'>
+            <h1>Death Note</h1>
+         </div>
+        </div>
+      </div>
+      <div className='container'>
+        <div className='divItem'>
+          <img src={GOT} alt="Game Of Thrones" />
+          <div className = 'overlay'>
+            <h1>Game Of Thrones</h1>
+         </div>
+        </div>
+        <div className='divItem'>
+          <img src={TheWire} alt="The Wire" />
+          <div className = 'overlay'>
+            <h1>The Wire</h1>
+         </div>
+        </div>
+        <div className='divItem'>
+          <img src={WalkingDead} alt="Walking Dead" />
+          <div className = 'overlay'>
+            <h1>Walking Dead</h1>
+         </div>
+        </div>
+      </div>
     </div>
   );
 }
